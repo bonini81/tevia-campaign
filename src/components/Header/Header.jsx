673350da846d1dc5eb8481eb05a5f33683071116
bonini81 <andresdominguez81@gmail.com>
2025-12-09 10:29:39 -0500

    Refactors

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-import logo from '../../assets/1-logo.png'; 
 import { MdVideocam } from "react-icons/md";
 import { FaHashtag } from "react-icons/fa";
 import { MdOutlineMessage } from "react-icons/md";
@@ -9,11 +8,11 @@ const Header = () => {
     <>
       <header> 
            <div className="container header-container">
-            <div>
-              <img src={logo} alt="Campaign Logo" className="logo" />
-            </div>
+            <h5 className="logo-header">
+              <span className="icono__yellow">#</span>LoQueImportaDeVerdad
+            </h5>
             <nav>
-            <a href="#que-importa"><FaHashtag className="icono__yellow" /> Lo qué importa</a>
+            <a href="#que-importa"><FaHashtag className="icono__yellow" /> Lo que importa</a>
                <a href="#testiominos"><MdVideocam className="icono__yellow" /> Testimonios</a>
               <a href="#brick-wall"><MdOutlineMessage className="icono__yellow" /> Muro</a>
            
